e810c57ff7ee56369189b107db40dcb28aa4ed26
James Henry <jhenry@vigosoftware.com>
2024-09-16 12:31:12 +0100

    update

--- build/Build.cs
+++ build/Build.cs
@@ -7,19 +7,15 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
+using Nuke.Common.Tools.DotNet;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    AbsolutePath Templates => RootDirectory / "templates/henry-js.Net.Templates.csproj";
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -39,6 +35,16 @@ class Build : NukeBuild
         .DependsOn(Restore)
         .Executes(() =>
         {
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetPack(_ => _
+                .SetProject(Templates)
+            );
+            
         });
 
 }
