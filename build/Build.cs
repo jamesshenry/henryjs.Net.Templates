475ac8856e7d58b1dc2f29a1599d78367777f3cc
henry-js <henry-js@outlook.com>
2024-09-16 23:14:33 +0100

    1.0.0

--- build/Build.cs
+++ build/Build.cs
@@ -11,16 +11,23 @@ using Nuke.Common.Tools.DotNet;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.MinVer;
+using Serilog;
+using System.IO;
 
 class Build : NukeBuild
 {
+    [MinVer] readonly MinVer MinVer;
     AbsolutePath TemplateProject => RootDirectory / "templates/henry-js.Net.Templates.csproj";
     AbsolutePath PackageDirectory => RootDirectory / "package";
     public static int Main() => Execute<Build>(x => x.Compile);
+    [Parameter] readonly string NugetApiKey;
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    bool IsMinVerPreRelease => MinVer.MinVerPreRelease is not null;
+    bool IsMinverRelease => MinVer.MinVerPreRelease is null;
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -42,11 +49,37 @@ class Build : NukeBuild
         .DependsOn(Restore)
         .Executes(() =>
         {
+            PackageDirectory.CreateOrCleanDirectory();
             DotNetTasks.DotNetPack(_ => _
                 .SetProject(TemplateProject)
                 .SetOutputDirectory(PackageDirectory)
             );
-            
         });
 
+    Target NugetUpload => _ => _
+        .OnlyWhenDynamic(() => IsMinverRelease)
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            Log.Information("MinVer.Version = {Value}", MinVer.Version);
+            Log.Information("MinVer.FileVersion = {Value}", MinVer.FileVersion);
+            Log.Information("MinVer.MinVerVersion = {Value}", MinVer.MinVerVersion);
+            Log.Information("MinVer.PackageVersion = {Value}", MinVer.PackageVersion);
+            Log.Information("MinVer.AssemblyVersion = {Value}", MinVer.AssemblyVersion);
+            Log.Information("MinVer.MinVerBuildMetadata = {Value}", MinVer.MinVerBuildMetadata);
+            Log.Information("MinVer.MinVerMajor = {Value}", MinVer.MinVerMajor);
+            Log.Information("MinVer.MinVerMinor = {Value}", MinVer.MinVerMinor);
+            Log.Information("MinVer.MinVerPatch = {Value}", MinVer.MinVerPatch);
+            Log.Information("MinVer.MinVerPreRelease = {Value}", MinVer.MinVerPreRelease);
+            Assert.NotNullOrWhiteSpace(NugetApiKey);
+            var packageFiles = PackageDirectory.GlobFiles("*.nupkg");
+            Assert.Count(packageFiles, 1, "Only 1 .nupkg file should be produced");
+            DotNetTasks.DotNetNuGetPush(_ => _
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetApiKey(NugetApiKey)
+                .CombineWith(packageFiles, (_, path) => _
+                    .SetTargetPath(path))
+                );
+
+        });
 }
