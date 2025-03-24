5b1f4b9e759e117ac63f0c765fd6f0ce28f76b77
James Henry <james.henry@kerridgecs.com>
2025-03-24 14:41:14 +0000

    build: use henryjs.Nuke build components

---
+++ .nuke/Build.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using henryjs.Nuke.Components;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.PathConstruction;
+
+class Build : NukeBuild, IPack
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(x => (x as ICompile).Compile);
+
+    string IHasMainProject.ProjectName => "henry-js.Net.Templates";
+
+}
