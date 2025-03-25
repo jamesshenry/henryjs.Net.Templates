d2fa8737a6512856902a2f9f50d459a7fd79ab3b
henry-js <henry-js@outlook.com>
2025-03-25 08:54:11 +0000

    remove obsolete templates

--- templates/content/csharp/ConsoleAppFrameworkConsole/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConsoleAppFramework;
-using ConsoleAppFrameworkConsole.Cli.Commands;
-using ConsoleAppFrameworkConsole.Cli.Extensions;
-
-MyServiceProvider sp = new();
-ConsoleApp.ServiceProvider = sp;
-var app = ConsoleApp.Create();
-app.Add<MyCommand>();
-await app.RunAsync(args);
